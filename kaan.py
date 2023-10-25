dabd6a5a4930dc4500f14bdf03caba2a314b2a6f
kaanozc11 <kaanozcantc54@gmail.com>
2023-10-25 22:16:07 +0300

    Create kaan.py

---
+++ kaan.py
@@ -0,0 +1,7 @@
+## This is a calculator that calculates your %40 midterm and %60 final score and prints your lecture score.
+
+print("Please follow the steps below.");
+mid = int(input("Please enter your midterm score."));
+final = int(input("Please enter your final score."));
+score = mid * 0.4 + final * 0.6;
+print(score);
